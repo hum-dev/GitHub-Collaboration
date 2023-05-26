66e9cde5622685d34763dc6f27e7683f84e303b7
hum-dev <humphreymuriungi@outlook.com>
2023-05-26 14:29:27 +0300

    first commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+
+
+function Input() {
+  return (
+    <div>
+
+       Input
+        
+
+    </div>
+  )
+}
+
+export default Input
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+
+function NavBar() {
+  return (
+    <div>
+        <header>
+            <h1>Collaboration</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Profile</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default NavBar
